49e302a03cbd96cc0893a8f65f072130858a94eb
Furhan Shabir <iamfurhan@gmail.com>
2019-01-17 17:00:58 +0900

    Fix syntax issues

--- src/ngx_http_vhost_traffic_status_string.c
+++ src/ngx_http_vhost_traffic_status_string.c
@@ -223,10 +223,10 @@ ngx_hex_encode_invalid_utf8_char(ngx_pool_t *pool, ngx_str_t *buf, u_char *p, si
             while (prev != p) {
                 c = *p;
                 
-                pb = (u_int)c >> 4;
+                pb = (u_char)(c >> 4);
                 pb++;
                 
-                pb = (u_int)c & 0x0f;
+                pb = (u_char)(c & 0x0f);
                 pb++;
 
                 prev++;
