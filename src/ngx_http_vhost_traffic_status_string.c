8b8e513a2a81524dc39fb67f2e29a64d0ff8d0a9
Furhan Shabir <furhan.shabir@rakuten.com>
2019-01-30 22:50:23 +0900

    Escape the non-utf encoded character

--- src/ngx_http_vhost_traffic_status_string.c
+++ src/ngx_http_vhost_traffic_status_string.c
@@ -202,6 +202,10 @@ ngx_hex_encode_invalid_utf8_char(ngx_pool_t *pool, ngx_str_t *buf, u_char *p, si
 
     /* Hex encoding will be at least twice the size of original string*/
     buf->data = ngx_pcalloc(pool, n * 4);
+
+    ngx_sprintf(buf->data, "%X", p);
+    return NGX_OK;
+
     size = 0;
     pb = buf->data;
 
